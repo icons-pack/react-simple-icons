2d56fa6eb07c619f03ff848367d8b914e7d612f3
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-01-21 23:59:40 -0600

    [react-simple-icons]: Update package simple-icons

---
+++ packages/react-simple-icons/src/Netapp.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Netapp = props => {
+  const { color, size, ...others } = props;
+  return (
+    <svg width={size} height={size} fill={color} viewBox="0 0 24 24" {...others}>
+      <path d="M0 2v20h9.33V10h5.34v12H24V2Z" />
+    </svg>
+  );
+};
+
+Netapp.propTypes = {
+  /**
+   * Hex color or color name
+   */
+  color: PropTypes.string,
+  /**
+   * The size of the Icon.
+   */
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+Netapp.defaultProps = {
+  color: 'currentColor',
+  size: '24',
+};
+
+export default Netapp;
