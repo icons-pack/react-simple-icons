76d1c3c439e3417e54a984ebb92f71e8f832404a
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-01-21 18:31:16 -0600

    [icons]: Update package simple-icons

---
+++ packages/react-simple-icons/src/Semanticweb.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Semanticweb = props => {
+  const { color, size, ...others } = props;
+  return (
+    <svg width={size} height={size} fill={color} viewBox="0 0 24 24" {...others}>
+      <path d="M21.602 0s-1.524 5.809-8.516 2.658c-.776-.35-.954-.444-.982-.469L2.074 6.301l10.043 4.896s.776-.326 2.026-.933C20.273 7.287 21.602 0 21.602 0zM1.59 8.486v10.448L10.947 24V13.242L1.59 8.486zm20.82 0l-9.357 4.756V24l9.357-5.066V8.486Z" />
+    </svg>
+  );
+};
+
+Semanticweb.propTypes = {
+  /**
+   * Hex color or color name
+   */
+  color: PropTypes.string,
+  /**
+   * The size of the Icon.
+   */
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+Semanticweb.defaultProps = {
+  color: 'currentColor',
+  size: '24',
+};
+
+export default Semanticweb;
