46ac5771e3818feeec0cf315aeee79f10a851288
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-05-01 16:58:38 -0500

    feat: update package simple-icons to v2.10.0

---
+++ packages/react-simple-icons/src/Guangzhoumetro.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Guangzhoumetro = props => {
+  const { color, size, ...others } = props;
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} viewBox="0 0 24 24" {...others}>
+      <path d="M16.433 12.329A16.188 16.188 0 0 1 22.118.009L17.684 0a16.2 16.2 0 0 0-4.776 11.374V24h3.525zm-8.869 0A16.174 16.174 0 0 0 1.882.009L6.319 0a16.238 16.238 0 0 1 4.773 11.374V24H7.564zm0 0Z" />
+    </svg>
+  );
+};
+
+Guangzhoumetro.propTypes = {
+  /**
+   * Hex color or color name
+   */
+  color: PropTypes.string,
+  /**
+   * The size of the Icon.
+   */
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+Guangzhoumetro.defaultProps = {
+  color: 'currentColor',
+  size: '24',
+};
+
+export default Guangzhoumetro;
