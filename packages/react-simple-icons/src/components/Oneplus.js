6bac7c66aa103dec06ee48ac81a9ebfa5faf68f2
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-05-18 11:19:14 -0500

    [simple-icons]: Update packages simple-icons

--- packages/react-simple-icons/src/components/Oneplus.js
+++ packages/react-simple-icons/src/components/Oneplus.js
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 
-const Adobe = forwardRef(function Adobe({ color = 'currentColor', size = 24, title = 'Adobe', ...others }, ref) {
+const Oneplus = forwardRef(function Oneplus({ color = 'currentColor', size = 24, title = 'OnePlus', ...others }, ref) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -12,12 +12,12 @@ const Adobe = forwardRef(function Adobe({ color = 'currentColor', size = 24, tit
       ref={ref}
       {...others}>
       <title>{title}</title>
-      <path d="M13.966 22.624l-1.69-4.281H8.122l3.892-9.144 5.662 13.425zM8.884 1.376H0v21.248zm15.116 0h-8.884L24 22.624Z" />
+      <path d="M0 3.74V24h20.26V12.428h-2.256v9.317H2.254V5.995h9.318V3.742zM18.004 0v3.74h-3.758v2.256h3.758v3.758h2.255V5.996H24V3.74h-3.758V0zm-6.45 18.756V8.862H9.562c0 .682-.228 1.189-.577 1.504-.367.297-.91.437-1.556.437h-.245v1.625h2.133v6.31h2.237z" />
     </svg>
   );
 });
 
-Adobe.propTypes = {
+Oneplus.propTypes = {
   /**
    * Hex color or color name
    */
@@ -32,4 +32,4 @@ Adobe.propTypes = {
   title: PropTypes.string,
 };
 
-export default Adobe;
+export default Oneplus;
