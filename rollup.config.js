c79d234ff0cc7c0c5e3c347b440bbf9c599573dc
Tommaso A <me@tommasoamici.com>
2022-08-12 08:07:33 +0200

    fix: rollup config prevented tree shaking (#141)
    * style: remove rollup configs from prettierignore
    
    * fix: rollup config prevented tree shaking
    
    Co-authored-by: Tommaso Amici <tamici@ripe.net>

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,69 @@
-import { createRollupConfig } from './scripts/createRollupConfig';
+import commonjs from '@rollup/plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+//import sourcemaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 import pkg from './package.json';
-
-const name = 'index';
 const scopeName = 'ReactSimpleIcons';
 
+const plugins = [
+  external(),
+  typescript({ clean: true }),
+  // sourcemaps(),
+];
+const terserPlugin = terser({
+  output: { comments: false },
+  compress: {
+    drop_console: true,
+  },
+});
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
 const options = [
   {
-    scopeName,
-    name,
-    format: 'cjs',
     input: pkg.source,
-    external: [],
     output: {
+      format: 'esm',
       globals: { react: 'React' },
+      dir: 'dist/esm',
+      preserveModules: true,
     },
+    plugins,
   },
   {
-    name,
-    format: 'esm',
     input: pkg.source,
-    external: [],
-    output: {
-      globals: { react: 'React' },
-    },
-  },
-  {
-    scopeName,
-    name,
-    format: 'umd',
-    input: pkg.source,
-    external: [],
-    output: {
-      globals: {
-        react: 'React',
+    output: [
+      {
+        format: 'cjs',
+        globals: { react: 'React' },
+        dir: 'dist/cjs',
+        preserveModules: true,
+        exports: 'auto',
       },
-    },
+    ],
+    plugins: [...plugins, terserPlugin],
+  },
+  {
+    input: pkg.source,
+    output: [
+      {
+        format: 'umd',
+        name: scopeName,
+        globals: { react: 'React' },
+        file: 'dist/index.umd.js',
+      },
+    ],
+    plugins: [
+      ...plugins,
+      commonjs({
+        include: /\/node_modules\//,
+      }),
+      terserPlugin,
+    ],
   },
 ];
 
-export default options.map(option => createRollupConfig(option));
+export default options;
