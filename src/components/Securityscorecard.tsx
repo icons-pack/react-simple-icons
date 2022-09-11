71e578476b1db1555c98413d3117dc81289aac3c
LitoMore <litomore@gmail.com>
2022-09-11 18:27:01 +0800

    feat(simple-icons): bump simple-icons@7.11.0

---
+++ src/components/Securityscorecard.tsx
@@ -0,0 +1,30 @@
+
+    import * as React from 'react';
+
+    export type SecurityscorecardProps = React.ComponentPropsWithoutRef<'svg'> & {
+      /**
+       * Hex color or color name
+       */
+      title?: string;
+      /**
+       * The size of the Icon.
+       */
+      color?: string;
+      /**
+       * The title provides an accessible short text description to the SVG
+       */
+      size?: string | number;
+    };
+
+    const Securityscorecard = React.forwardRef<SVGSVGElement, SecurityscorecardProps>(function Securityscorecard({color = 'currentColor', size = 24, title = "securityscorecard", ...others}, ref) {
+
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} viewBox="0 0 24 24" ref={ref} {...others}>
+          <title>{title}</title>
+          <path d="M16.3696 2.5006 12.0006 5 7.6303 7.5006v-5L12.0006 0Zm6.1177 3.499.0028 4.986-8.7282-4.9929 4.3564-2.4923Zm-4.369 9.5085-.0014 4.9972 4.3774-2.5007-.0028-5.018-4.3732-2.502zM7.6274 21.502 12.0006 24l4.369-2.4952v-4.9972zM7.6303 9.5v5.0014l4.3703 2.4992 4.369-2.4937V9.5001l-4.369-2.4993Zm-6.1248 8.5044.0028-5.0055 8.7464 5.0027-4.376 2.5008Zm4.376-14.504L1.5125 6.001l-.0028 4.9985 4.3718 2.502z" />
+        </svg>
+      );
+    });
+
+    export default Securityscorecard
+  
